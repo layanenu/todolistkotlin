6f348af24dbe69004b0117db16aa0fd5e2342fd5
layanenu <layanenu@gmail.com>
2025-02-23 18:29:01 -0300

    feat: finish project

---
+++ app/src/main/java/dev/layane/todolist/ProfileActivity.kt
@@ -0,0 +1,35 @@
+package dev.layane.todolist
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import dev.layane.todolist.utils.AuthUtils
+import dev.layane.todolist.utils.Navigation
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        val homeBtn = findViewById<ImageView>(R.id.home)
+        val logoutBtn = findViewById<ImageView>(R.id.logout)
+        val saveBtn = findViewById<Button>(R.id.save_btn)
+
+        saveBtn.setOnClickListener {
+            Toast.makeText(this, "Perfil salvo", Toast.LENGTH_SHORT).show()
+            android.os.Handler().postDelayed({
+                Navigation.goToScreen(this, MainActivity::class.java)
+            }, 5000)
+        }
+
+        homeBtn.setOnClickListener {
+            Navigation.goToScreen(this, MainActivity::class.java)
+        }
+
+        logoutBtn.setOnClickListener {
+            AuthUtils.logout(this);
+        }
+    }
+}
---
+++ app/src/main/java/dev/layane/todolist/RegisterActivity.kt
@@ -0,0 +1,62 @@
+package dev.layane.todolist
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import dev.layane.todolist.utils.Navigation
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class RegisterActivity : AppCompatActivity() {
+    val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        val btnRegister = findViewById<Button>(R.id.submitRegisterBtn)
+        val forgotPassword = findViewById<TextView>(R.id.forgotPassword)
+        val LoginLink = findViewById<TextView>(R.id.login)
+
+        btnRegister.setOnClickListener {
+            val email = findViewById<TextView>(R.id.emailInput).text.toString()
+            val password = findViewById<TextView>(R.id.passwordInput).text.toString()
+
+            CoroutineScope(Dispatchers.IO).launch {
+                register(email, password)
+            }
+        }
+
+        forgotPassword.setOnClickListener {
+            Navigation.goToScreen(this, ForgotPasswordActivity::class.java)
+        }
+
+        LoginLink.setOnClickListener {
+            Navigation.goToScreen(this, LoginActivity::class.java)
+        }
+    }
+
+    fun register(email: String, password: String) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        baseContext,
+                        "Usuário cadastrado com sucesso!!!",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    Navigation.goToScreen(this, MainActivity::class.java)
+                } else {
+                    Toast.makeText(
+                        baseContext,
+                        "Falha ao registrar usuário.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+    }
+}
